4522092a7b514127930f9a45ddafd660aecae7e1
pathfinder1549 <mhilliard117@gmail.com>
2022-01-03 18:48:25 -0500

    ex1

---
+++ exercises/worksheet.py
@@ -0,0 +1,12 @@
+
+
+
+# new_list = [new_item for item in list]
+
+numbers = [1, 2, 3]
+new_list = [n+1 for n in numbers]
+print(new_list)
+
+name = "Jojo"
+new_list = [letter for letter in name]
+print(new_list)
